91d40a49b25bec1f0250dc5e57d746df5ed114e4
Cesar Campos <cesarorlandocampos@gmail.com>
2021-04-05 22:01:28 -0500

    initial commit and adding files to repo

---
+++ test/intern.test.js
@@ -0,0 +1,26 @@
+const Employee = require("./Employee.js"); //need to make it as not made
+//{id: name: email:}
+describe("Employee constructor ", () => {
+    describe("", () => {
+
+        it("can be an instance of the EMployee Class", () => {
+            expect(typeof new Employee()).toBe("object");
+        })
+
+        it("should have a name property", () => {
+            const name = "Rachel";
+            const newEmployee = new Employee(1, name, "email@email.com")
+
+            expect(newEmployee.name).toBe(name)
+        })
+    })
+
+    describe("getName function", () => {
+        it("should retrieve the name propertys value of each instance of an employee", () => {
+            const name = "Rachel";
+            const newEmployee = new Employee(1, name, "email@email.com")
+
+            expect(newEmployee.getName()).toBe(name)
+        })
+    })
+})
